f737b17a9ffc77b8d345a94de98b79a40e4330ab
HuntCode <wanglv666@gmail.com>
2026-01-28 14:00:28 +0800

    1.提交基础版本

---
+++ src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func startHttp(port string) {
+	fmt.Printf("Start http port: %s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func startHttps(port, cert, key string) {
+	fmt.Printf("Start https port: %s\n", port)
+	err := http.ListenAndServeTLS(port, cert, key, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	staticUrl := "/static/"
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle(staticUrl, http.StripPrefix(staticUrl, fs))
+
+	go startHttp(":8080")
+
+	startHttps(":8081", "./conf/server_fullchain.crt", "./conf/server.key")
+}

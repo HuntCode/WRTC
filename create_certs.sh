e85c04e42273e8667b54f9bcde03a5a3db3e5b9e
HuntCode <wanglv666@gmail.com>
2026-01-28 15:00:13 +0800

    1.删除原结构文件

--- create_certs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -out ca.key 2048
-openssl req -x509 -new -key ca.key -out ca.crt
-openssl genrsa -out server.key 2048
-openssl req -new -key server.key -out server.csr
-openssl x509 -req -sha256 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 3650 -out server.crt

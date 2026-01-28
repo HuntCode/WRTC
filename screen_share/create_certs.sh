2982f9f30fa44d67df7b34a15ed31609e917e91f
HuntCode <wanglv666@gmail.com>
2026-01-28 14:57:12 +0800

    1.调整仓库结构

---
+++ screen_share/create_certs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+openssl genrsa -out ca.key 2048
+openssl req -x509 -new -key ca.key -out ca.crt
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -sha256 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 3650 -out server.crt
